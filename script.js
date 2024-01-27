00ff697f57d0e7a80ee955646e5541272fb6e577
Pratik <wrickguha2003@gmail.com>
2024-01-28 04:36:01 +0530

    Tired as fuck

---
+++ script.js
@@ -0,0 +1,14 @@
+let user_score=0;
+let com_score=0;
+
+const choice= document.querySelectorAll(".image");
+
+choice.forEach((image) =>
+{
+    image.addEventListener("click", () =>
+    {
+        const user_choice=choice.getAttribute("id");
+        console.log("It is clicked",user_choice);
+    })
+}
+)
