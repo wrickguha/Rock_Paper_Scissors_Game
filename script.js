9ff50bbd7919343d2f37172f3048ee25e76e6f4c
Pratik <wrickguha2003@gmail.com>
2024-02-03 22:59:03 +0530

    Added another feature

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-let user_score=0;
-let com_score=0;
+let user_score = 0;
+let com_score = 0;
 
-const choice= document.querySelectorAll(".image");
+const gen_com_choice = () => {
+    const options = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return options[randomNum];
+}
 
-choice.forEach((image) =>
-{
-    image.addEventListener("click", () =>
-    {
-        const user_choice=image.getAttribute("id");
-        console.log("It is clicked",user_choice);
+const play_game = (user_choice) => {
+    console.log("User choice: ", user_choice);
+    //Generate computer's choice
+    const comm_choice = gen_com_choice();
+
+    console.log("Computer choice: ", comm_choice);
+
+
+    if (user_choice === comm_choice) {
+        console.log("The game is draw");
+    } else {
+        if ((user_choice === 'rock') && (comm_choice === 'scissors')) {
+            user_score++;
+            console.log('You win this round');
+        }
+        else if ((user_choice === 'rock') && (comm_choice === 'paper')) {
+            com_score++
+            console.log('You lose this round');
+        }
+        else if ((user_choice === 'paper') && (comm_choice === 'scissors')) {
+            user_score++;
+            console.log('You lose this round');
+            com_score++;
+        }
+
+
+    }
+}
+
+
+
+const choice = document.querySelectorAll(".image");
+
+choice.forEach((image) => {
+    image.addEventListener("click", () => {
+        const user_choice = image.getAttribute("id");
+        play_game(user_choice);
     })
 }
 )
