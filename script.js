e721eb6f3d5e7dcbf8a12a3509babe7093d0d60b
Pratik <wrickguha2003@gmail.com>
2024-02-05 19:44:33 +0530

    Bugs are never Ending

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let user_score = 0;
 let com_score = 0;
 
+
+const user_score_para=document.querySelector("#person");
+const com_score_para=document.querySelector("#computer");
+
 const gen_com_choice = () => {
     const options = ['rock', 'paper', 'scissors'];
     const randomNum = Math.floor(Math.random() * 3);
     return options[randomNum];
 }
+
+
+const draw_game = () => {
+    msg_con.innerText = "Game is draw, Play Again.";
+}
+
+const show_winner=(user_score,com_score)=>
+{
+    if(user_score++)
+    {
+        msg_con.innerText="You Won the Game";
+        user_score_para.innerText=user_score;
+    }
+    else
+    {
+        msg_con.innerText="You lose the Game";
+        com_score_para.innerText=com_score;
+    }
+}
+
+
 
 const play_game = (user_choice) => {
     console.log("User choice: ", user_choice);
@@ -17,21 +42,38 @@ const play_game = (user_choice) => {
 
     if (user_choice === comm_choice) {
         console.log("The game is draw");
+
+        draw_game();
+
     } else {
+        
         if ((user_choice === 'rock') && (comm_choice === 'scissors')) {
             user_score++;
             console.log('You win this round');
+            
         }
         else if ((user_choice === 'rock') && (comm_choice === 'paper')) {
-            com_score++
+            com_score++;
             console.log('You lose this round');
         }
         else if ((user_choice === 'paper') && (comm_choice === 'scissors')) {
-            user_score++;
             console.log('You lose this round');
             com_score++;
         }
-
+        else if ((user_choice === 'paper') && (comm_choice === 'rock')) {
+            user_score++;
+            console.log('You win this round');
+        }
+        else if (user_choice === 'scissors' && comm_choice === 'paper') {
+            user_score++;
+            console.log("You win this round");
+        }
+        else if (user_choice === 'scissors' && comm_choice === 'rock') {
+            com_score++;
+            console.log("You lose this round");
+        }
+    
+        show_winner(user_score,com_score);
 
     }
 }
