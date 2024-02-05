7e2b4f51e09e67c2e11c36227829fb753edbedd9
Pratik <wrickguha2003@gmail.com>
2024-02-05 23:52:32 +0530

    Can't fix the final bug

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let user_score = 0;
 let com_score = 0;
 
 
-const user_score_para=document.querySelector("#person");
-const com_score_para=document.querySelector("#computer");
+const msg_con = document.querySelector("#msg_con");
+
+const user_score_para = document.querySelector("#person");
+const com_score_para = document.querySelector("#computer");
 
 const gen_com_choice = () => {
     const options = ['rock', 'paper', 'scissors'];
@@ -14,19 +16,19 @@ const gen_com_choice = () => {
 
 const draw_game = () => {
     msg_con.innerText = "Game is draw, Play Again.";
+    msg_con.style.backgroundColor = "#E56B6F";
 }
 
-const show_winner=(user_score,com_score)=>
-{
-    if(user_score++)
-    {
-        msg_con.innerText="You Won the Game";
-        user_score_para.innerText=user_score;
+const show_winner = (user_score, com_score) => {
+    if (user_score>com_score) {
+        msg_con.innerText = "You Won the Game";
+        user_score_para.innerText = user_score;
+        msg_con.style.backgroundColor = "#EAAC8B";
     }
-    else
-    {
-        msg_con.innerText="You lose the Game";
-        com_score_para.innerText=com_score;
+    else if(com_score>user_score) {
+        msg_con.innerText = "You lose the Game";
+        com_score_para.innerText = com_score;
+        msg_con.style.backgroundColor = "#355070";
     }
 }
 
@@ -46,11 +48,11 @@ const play_game = (user_choice) => {
         draw_game();
 
     } else {
-        
+
         if ((user_choice === 'rock') && (comm_choice === 'scissors')) {
             user_score++;
             console.log('You win this round');
-            
+
         }
         else if ((user_choice === 'rock') && (comm_choice === 'paper')) {
             com_score++;
@@ -72,8 +74,8 @@ const play_game = (user_choice) => {
             com_score++;
             console.log("You lose this round");
         }
-    
-        show_winner(user_score,com_score);
+
+        show_winner(user_score, com_score);
 
     }
 }
